0ed0fad92c67a762fb22a90e38e41bb1da513651
Denis Fadeev <denis@fadeev.org>
2020-05-28 10:57:21 +0500

    init

---
+++ x/checklist/alias.go
@@ -0,0 +1,40 @@
+package checklist
+
+import (
+	"github.com/fadeev/checklist-chain/x/checklist/keeper"
+	"github.com/fadeev/checklist-chain/x/checklist/types"
+)
+
+const (
+	// TODO: define constants that you would like exposed from your module
+
+	ModuleName        = types.ModuleName
+	RouterKey         = types.RouterKey
+	StoreKey          = types.StoreKey
+	DefaultParamspace = types.DefaultParamspace
+	QueryParams       = types.QueryParams
+	QuerierRoute      = types.QuerierRoute
+)
+
+var (
+	// functions aliases
+	NewKeeper           = keeper.NewKeeper
+	NewQuerier          = keeper.NewQuerier
+	RegisterCodec       = types.RegisterCodec
+	NewGenesisState     = types.NewGenesisState
+	DefaultGenesisState = types.DefaultGenesisState
+	ValidateGenesis     = types.ValidateGenesis
+	// TODO: Fill out function aliases
+
+	// variable aliases
+	ModuleCdc     = types.ModuleCdc
+	// TODO: Fill out variable aliases
+)
+
+type (
+	Keeper       = keeper.Keeper
+	GenesisState = types.GenesisState
+	Params       = types.Params
+
+	// TODO: Fill out module types
+)
---
+++ x/checklist/client/cli/query.go
@@ -0,0 +1,38 @@
+package cli
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/spf13/cobra"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/client/context"
+	"github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/fadeev/checklist-chain/x/checklist/types"
+)
+
+// GetQueryCmd returns the cli query commands for this module
+func GetQueryCmd(queryRoute string, cdc *codec.Codec) *cobra.Command {
+	// Group checklist queries under a subcommand
+	checklistQueryCmd := &cobra.Command{
+		Use:                        types.ModuleName,
+		Short:                      fmt.Sprintf("Querying commands for the %s module", types.ModuleName),
+		DisableFlagParsing:         true,
+		SuggestionsMinimumDistance: 2,
+		RunE:                       client.ValidateCmd,
+	}
+
+	checklistQueryCmd.AddCommand(
+		flags.GetCommands(
+	// TODO: Add query Cmds
+		)...,
+	)
+
+	return checklistQueryCmd
+}
+
+// TODO: Add Query Commands
