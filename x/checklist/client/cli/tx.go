571e81652178ede2229d72c49cd742a2fba8c9b4
Denis Fadeev <denis@fadeev.org>
2020-05-30 13:00:00 +0500

    Imports for querier.go

--- x/checklist/client/cli/tx.go
+++ x/checklist/client/cli/tx.go
@@ -1,13 +1,17 @@
 package cli
 
 import (
+	// "bufio"
 	"fmt"
 
 	"github.com/spf13/cobra"
 
 	"github.com/cosmos/cosmos-sdk/client"
+	// "github.com/cosmos/cosmos-sdk/client/context"
 	"github.com/cosmos/cosmos-sdk/client/flags"
+	// "github.com/cosmos/cosmos-sdk/client/utils"
 	"github.com/cosmos/cosmos-sdk/codec"
+	// "github.com/cosmos/cosmos-sdk/x/auth"
 	"github.com/fadeev/checklist-chain/x/checklist/types"
 )
 
